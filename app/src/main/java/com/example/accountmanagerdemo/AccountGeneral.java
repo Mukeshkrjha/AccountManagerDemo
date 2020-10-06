65d1d637e058df8941d720d8ffcc8558c4ccd64e
Mukesh Jha <mukeshmca.jha@gmail.com>
2020-10-07 00:48:54 +0530

    initial commit

---
+++ app/src/main/java/com/example/accountmanagerdemo/AccountGeneral.java
@@ -0,0 +1,24 @@
+package com.example.accountmanagerdemo;
+
+public class AccountGeneral {
+
+    /**
+     * Account type id
+     */
+    public static final String ACCOUNT_TYPE = "com.example.accountmanagerdemo";
+
+    /**
+     * Account name
+     */
+    public static final String ACCOUNT_NAME = "DEMO";
+
+    /**
+     * Auth token types
+     */
+    public static final String AUTHTOKEN_TYPE_READ_ONLY = "Read only";
+    public static final String AUTHTOKEN_TYPE_READ_ONLY_LABEL = "Read only access to an account";
+
+    public static final String AUTHTOKEN_TYPE_FULL_ACCESS = "Full access";
+    public static final String AUTHTOKEN_TYPE_FULL_ACCESS_LABEL = "Full access to an account";
+
+}
---
+++ app/src/main/java/com/example/accountmanagerdemo/AuthenticatorService.java
@@ -0,0 +1,14 @@
+package com.example.accountmanagerdemo;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class AuthenticatorService extends Service {
+    @Override
+    public IBinder onBind(Intent intent) {
+
+        Authenticator authenticator = new Authenticator(this);
+        return authenticator.getIBinder();
+    }
+}
